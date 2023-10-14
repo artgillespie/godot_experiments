3012511e1cbcc66e417e14cf5a561553f930d533
Art <art@tapsquare.com>
2023-10-13 19:04:14 -0700

    added labels for control points in bezier viz

--- control_point.gd
+++ control_point.gd
@@ -12,11 +12,16 @@ var is_mouse_over: bool = false
 @onready var _default_font: Font = ThemeDB.fallback_font
 @onready var _default_font_size: int = ThemeDB.fallback_font_size
 
+func _ready():
+	$CanvasLayer/Label.text = name
 
 func _draw():
 	var color = Color.RED if is_mouse_over else Color.WHITE
 	draw_rect(Rect2(-size.x/2, -size.y/2, size.x, size.y), color)
 	draw_rect(Rect2(-size.x/2, -size.y/2, size.x, size.y), Color.BLACK, false, 0.1)
+	draw_set_transform(Vector2(0,0), 0.0, Vector2(1/32., -1/32.))
+	draw_string(_default_font, Vector2(-40, 0.0), name, HORIZONTAL_ALIGNMENT_LEFT, -1, 32)
+	draw_set_transform(Vector2(0,0), 0.0, Vector2(32., -32.))
 
 func _on_area_2d_mouse_entered():
 	is_mouse_over = true
